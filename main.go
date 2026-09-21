e0ed8a2e5dad36589f12d353998a998c3efee2ce
Ezekiel Daniels <ezekieldaniels@my.unt.edu>
2026-09-20 20:13:21 -0500

    Created HTTP server with stand in functions

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from my JWKS server!")
+}
 
 func main() {
-    fmt.Println("Hello, JWKS server!")
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
